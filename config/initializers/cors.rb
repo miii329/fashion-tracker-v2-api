38c238fbc12646fed78691deb9b16434e45610eb
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-29 12:42:47 +0900

    style: Rubocop自動修正を適用

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fashion-trends-tracker.web.app', # 本番環境
-            'http://localhost:4200',                   # ローカル開発（Angular）
-            'http://localhost:3000'                    # ローカル開発（React等）
+    origins "https://fashion-trends-tracker.web.app", # 本番環境
+            "http://localhost:4200",                   # ローカル開発（Angular）
+            "http://localhost:3000"                    # ローカル開発（React等）
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v2 do
       # ログイン・ログアウト
-      resource :session, only: [:create, :destroy]
+      resource :session, only: [ :create, :destroy ]
       # ユーザー登録
-      resource :users, only: [:create] # これが「ユーザー登録」の宛先
+      resource :users, only: [ :create ] # これが「ユーザー登録」の宛先
       # パスワードリセット
-      resources :passwords, param: :token, only: [:create, :update]
+      resources :passwords, param: :token, only: [ :create, :update ]
       # アイテム一覧・詳細
-      resources :items, only: [:index, :show]
+      resources :items, only: [ :index, :show ]
     end
   end
 
